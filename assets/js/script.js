21ceb7f12eafe0623d333ea90ede9d30370f3e8d
Marija Nastasijevic <nastasijevicmarija27@gmail.com>
2021-10-07 12:33:56 +0200

    Add new products, add printing products

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,26 @@
 'use strict';
 $(document).ready(function () {
 
-    ajaxCallback("livingroom.json", function (result) {
-        let arrayLivingroom = result;
-        setDataLS("livingroomLS", arrayLivingroom);
-        createList(arrayLivingroom, "Livingroom", "ddlLivingroom", "#ddlLivingRoomBlock")
+    ajaxCallback("rooms.json", function (result) {
+        let arrayRooms = result;
+        setDataLS("roomsLS", arrayRooms);
+        createList(arrayRooms, "Rooms", "ddlRooms", "#ddlRoomsBlock")
     });
 
-    ajaxCallback("bedroom.json", function (result) {
-        let arrayBedroom = result;
-        setDataLS("bedroomLS", arrayBedroom);
-        createList(arrayBedroom, "Bedroom", "ddlBedroom", "#ddlBedRoomBlock")
+    ajaxCallback("type.json", function (result) {
+        let arrayType = result;
+        setDataLS("typeLS", arrayType);
+        createList(arrayType, "Type furniture", "ddlType", "#ddlTypeBlock")
     });
 
-    ajaxCallback("entranceHall.json", function (result) {
-        let arrayEntranceHall = result;
-        setDataLS("entranceHallLS", arrayEntranceHall);
-        createList(arrayEntranceHall, "Entrance hall", "ddlEntranceHall", "#ddlEntranceHallBlock")
-    });
-
-    ajaxCallback("kitchen.json", function (result) {
-        let arrayKitchen = result;
-        setDataLS("kitchenLS", arrayKitchen);
-        createList(arrayKitchen, "Kitchen", "ddlKitchen", "#ddlKitchenBlock")
-    });
-
-    ajaxCallback("diningRoom.json", function (result) {
-        let arrayDiningRoom = result;
-        setDataLS("diningRoomLS", arrayDiningRoom);
-        createList(arrayDiningRoom, "Dining room", "ddlDiningRoom", "#dinningRoomBlock")
+    ajaxCallback("products.json", function (result) {
+        let arrayProducts = result;
+        setDataLS("productsLS", arrayProducts);
+        //printProducts(arrayProducts)
     })
 
-    ajaxCallback("childrensRoom.json", function (result) {
-        let arrayChildrensRoom = result;
-        setDataLS("childrensRoomLS", arrayChildrensRoom);
-        createList(arrayChildrensRoom, "Children's room", "ddlChildrensRoom", "#ChildrensRoomBlock")
-    })
-
-
-
-
-
-
+    $(document).on('change', '#ddlRooms', filtSort);
+    $(document).on('change', '#ddlType', filtSort);
 
 
 })
@@ -83,6 +62,78 @@ function createList(data, textLabel, idList, idBlock) {
     $(idBlock).html(printHtml);
 
 }
+
+function printProducts(products) {
+    let printHtml = "";
+
+    if (products.length == 0) {
+        printHtml += `<div class = "col-12><p>Trenutno nema proizvoda</p></div>"`
+    }
+    else {
+        for (let objProduct of products) {
+            printHtml += `<div class = "col-3" >
+                            <h5><strong>${returnName(objProduct.typeID, "typeLS")}</strong></h5>
+
+                            <h4>${objProduct.name}</h4>
+                            
+                            <img src="${objProduct.image.src}" alt="${objProduct.image.alt}" class="img-fluid" />
+                        </div>`
+
+        }
+    }
+    console.log(printHtml);
+    $("#productsBlock").html(printHtml);
+}
+
+function returnName(id, nameLS) {
+    let name = "";
+
+    let data = returnDataLS(nameLS);
+
+    if (data == null) {
+        name = "/"
+    }
+    else {
+        for (let obj of data) {
+            if (obj.id == id) {
+                name = obj.key
+            }
+        }
+    }
+    return name;
+}
+
+function filtSort() {
+    let roomsID = $('#ddlRooms').val();
+    let typeID = $('#ddlType').val();
+    console.log("roomsID: " + roomsID);
+    console.log("typeID: " + typeID);
+
+    let arrayProducts = returnDataLS("productsLS");
+
+    let filteredProducts = filter(arrayProducts, roomsID, typeID);
+
+    //sort(filteredProducts);
+    printProducts(filteredProducts);
+}
+
+function filter(products, roomID, typeID) {
+    let filteredProducts = [];
+    for (let product of products) {
+        if (product.roomID == roomID && product.typeID == typeID) {
+            filteredProducts.push(product);
+        }
+    }
+
+    return filteredProducts;
+}
+
+function sort(products) {
+
+}
+
+
+
 
 
 
